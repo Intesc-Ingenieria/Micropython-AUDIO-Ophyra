fe4eda5f7de9d3d5a55c022d74fde2c896393644
JavierMF22 <88099461+JavierMF22@users.noreply.github.com>
2021-12-01 19:49:45 -0600

    Update wav_ophyra.py

--- amplificador_ophyra/wav_ophyra.py
+++ amplificador_ophyra/wav_ophyra.py
@@ -15,10 +15,10 @@ def play(WAV_FILE, seg):
     SAMPLE_RATE_IN_HZ = 8000    #Tasa de muestreo en Hz (recomendado a 8 KHz o 16 KHz)
 
     print("-------- INICIO DE AUDIO --------")
-    lon = 44  #Se ignora el encabezado del .WAV
+    ini = 44  #Se ignora el encabezado del .WAV
     for i in range(seg):
         wav = open(WAV_FILE, "rb") #Apertura del archivo de audio 
-        pos = wav.seek(lon)
+        pos = wav.seek(ini)
 
         #---- Declaración de buffers de datos ----
         wav_samples = bytearray(SAMPLE_RATE_IN_HZ)  #Matriz de bytes equivalentes a 1 seg de reproducción
@@ -28,14 +28,14 @@ def play(WAV_FILE, seg):
         try:
             num_read =wav.readinto(wav_samples_mv) #Se escriben los datos del .WAV al buffer de datos
             if num_read == 0:
-                pos = wav.seek(lon) #Se ignora el encabezado del .WAV
+                pos = wav.seek(ini) #Se ignora el encabezado del .WAV
             else:
                 dac.write_timed(wav_samples_mv[:num_read], SAMPLE_RATE_IN_HZ, mode=DAC.CIRCULAR) #Se escriben los datos del buffer al DAC
         except (KeyboardInterrupt, Exception) as e:
             print("caught exception {} {}".format(type(e).name, e))
 
         print("Reproduciendo... " + str(i) +" seg")
-        lon = lon + SAMPLE_RATE_IN_HZ  #Se mueve el cursor para leer el siguiente segundo del archivo .WAV
+        ini = ini + SAMPLE_RATE_IN_HZ  #Se mueve el cursor para leer el siguiente segundo del archivo .WAV
         wav.close() #Se cierra el archivo de audio
         delay(1000)  #Espera 1 seg, tiempo para reproducir el sonido en el amplificador
         
