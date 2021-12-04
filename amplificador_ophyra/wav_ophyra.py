cd5bb828516b2016501b6ebff762b97a4e793650
JavierMF22 <88099461+JavierMF22@users.noreply.github.com>
2021-12-03 20:16:38 -0600

    Update wav_ophyra.py

--- amplificador_ophyra/wav_ophyra.py
+++ amplificador_ophyra/wav_ophyra.py
@@ -8,36 +8,35 @@
 from pyb import DAC
 from pyb import delay
 
-def play(WAV_FILE, seg):
+class wav:
 
-    #-------------CONFIGURACIÓN DE AUDIO---------------
-    dac = DAC(1, bits=8)        #Inicialización del DAC1 en modo de 8 bits / 8 bits de resolución.
-    SAMPLE_RATE_IN_HZ = 8000    #Tasa de muestreo en Hz (recomendado a 8 KHz o 16 KHz)
+    def play(WAV_FILE, seg, canal, muestreo):
+        #-------------CONFIGURACIÓN DE AUDIO---------------
+        dac = DAC(canal, bits=8)        #Inicialización del DAC en modo de 8 bits / 8 bits de resolución.
+        SAMPLE_RATE_IN_HZ = muestreo    #Tasa de muestreo en Hz
 
-    print("-------- INICIO DE AUDIO --------")
-    ini = 44  #Se ignora el encabezado del .WAV
-    for i in range(seg):
-        wav = open(WAV_FILE, "rb") #Apertura del archivo de audio 
-        pos = wav.seek(ini)
+        #------- Inicio de reproducción -------
+        ini = 44  #Se ignora el encabezado del .WAV
+        for i in range(seg+1):
+            wav = open(WAV_FILE, "rb") #Apertura del archivo de audio 
+            pos = wav.seek(ini)
 
-        #---- Declaración de buffers de datos ----
-        wav_samples = bytearray(SAMPLE_RATE_IN_HZ)  #Matriz de bytes equivalentes a 1 seg de reproducción
-        wav_samples_mv = memoryview(wav_samples)
+            #---- Declaración de buffers de datos ----
+            wav_samples = bytearray(SAMPLE_RATE_IN_HZ)  #Matriz de bytes equivalentes a 1 seg de reproducción
+            wav_samples_mv = memoryview(wav_samples)
 
-        #---- Inicio de reproducción del archivo ----
-        try:
-            num_read =wav.readinto(wav_samples_mv) #Se escriben los datos del .WAV al buffer de datos
-            if num_read == 0:
-                pos = wav.seek(ini) #Se ignora el encabezado del .WAV
-            else:
-                dac.write_timed(wav_samples_mv[:num_read], SAMPLE_RATE_IN_HZ, mode=DAC.CIRCULAR) #Se escriben los datos del buffer al DAC
-        except (KeyboardInterrupt, Exception) as e:
-            print("caught exception {} {}".format(type(e).name, e))
+            #---- Escritura en buffer DAC ----
+            try:
+                num_read =wav.readinto(wav_samples_mv) #Se escriben los datos del .WAV al buffer de datos
+                if num_read == 0:
+                    pos = wav.seek(ini) #Se ignora el encabezado del .WAV
+                else:
+                    dac.write_timed(wav_samples_mv[:num_read], SAMPLE_RATE_IN_HZ, mode=DAC.CIRCULAR) #Se escriben los datos del buffer al DAC
+            except (KeyboardInterrupt, Exception) as e:
+                print("caught exception {} {}".format(type(e).name, e))
 
-        print("Reproduciendo... " + str(i) +" seg")
-        ini = ini + SAMPLE_RATE_IN_HZ  #Se mueve el cursor para leer el siguiente segundo del archivo .WAV
-        wav.close() #Se cierra el archivo de audio
-        delay(1000)  #Espera 1 seg, tiempo para reproducir el sonido en el amplificador
-        
-    print("--------- FIN DE AUDIO ---------")
-    dac.deinit()  #Se desactiva el DAC
+            print("Reproduciendo... " + str(i) +" seg")
+            ini = ini + SAMPLE_RATE_IN_HZ  #Se mueve el cursor para leer el siguiente segundo del archivo .WAV
+            wav.close() #Se cierra el archivo de audio
+            delay(1000)  #Espera 1 seg, tiempo para reproducir el sonido en el amplificador
+        dac.deinit()  #Se desactiva el DAC
